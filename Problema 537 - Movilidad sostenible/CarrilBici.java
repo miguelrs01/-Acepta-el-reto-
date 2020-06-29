8defaa66e6ef661e0516bffe8bc2fd96ea6f32ac
Miguel Rodríguez S <57198803+miguelrs01@users.noreply.github.com>
2020-06-29 13:57:18 +0200

    Update CarrilBici.java

--- Problema 537 - Movilidad sostenible/CarrilBici.java
+++ Problema 537 - Movilidad sostenible/CarrilBici.java
@@ -12,8 +12,10 @@ public class CarrilBici
     public static void main (String[] args) throws java.io.IOException
     {
         String linea;
+        ArrayList puntosRecorridos = new ArrayList<>();
+        boolean correcto = true;
         try {
-        BufferedReader br = new BufferedReader (new FileReader ("sample.in"));
+        BufferedReader br = new BufferedReader (new FileReader ("/home/gandalf/Desktop/DATOS.txt"));
         //leo la linea y compruebo que no esta vacia (fin del fichero)
         while((linea = br.readLine())!=null){
             StringTokenizer st = new StringTokenizer (linea);
@@ -21,20 +23,41 @@ public class CarrilBici
             String p = st.nextToken();
             String c = st.nextToken();
             
+            puntosRecorridos.clear();
             int puntos = Integer.parseInt(p);
             int carriles = Integer.parseInt(c);
             
             if( carriles < (puntos-1)) { 
                 System.out.println("A PIE");
-            } else {
-                System.out.println("BICI");
-            }
-            
-            for(int i = 0; i < carriles; i++) {
+                for(int i = 0; i < carriles; i++) {
                 br.readLine();
-            }
+               }
+            } else {
+                for(int i = 0; i < carriles; i++) {
+                linea = br.readLine();
+                st = new StringTokenizer (linea);
             
+                String p1 = st.nextToken();
+                String p2 = st.nextToken();
+                int a = Integer.parseInt(p1);
+                int b = Integer.parseInt(p2);
+                
+                puntosRecorridos.add(a);
+                puntosRecorridos.add(b);
+               }
+               
+               Collections.sort(puntosRecorridos);
+               Set<String> hashSet = new HashSet<String>(puntosRecorridos);
+               puntosRecorridos.clear();
+               puntosRecorridos.addAll(hashSet);
+               if (puntosRecorridos.size() < puntos){
+                   System.out.println("A PIE");
+                } else {
+                    System.out.println("BICI");
+                }
+            }
+
         }
-      } catch(Exception e) {}
+       }catch(Exception e) {}
     }
 }
