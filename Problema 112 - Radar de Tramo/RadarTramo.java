6b411ff1242f286454a93a0168685742457d70ea
Miguel Rodríguez S <57198803+miguelrs01@users.noreply.github.com>
2022-01-10 12:23:25 +0100

    Update RadarTramo.java

--- Problema 112 - Radar de Tramo/RadarTramo.java
+++ Problema 112 - Radar de Tramo/RadarTramo.java
@@ -17,43 +17,43 @@ public class RadarTramo
         // Cargo el buffer con el contenido del archivo
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //leo la linea y compruebo que no esta vacia (fin del fichero)
-        while((linea = br.readLine())!=null){
+        while((linea = br.readLine())!=null)
+        {
            
                StringTokenizer st = new StringTokenizer (linea);
                seguir = true;
-               // bucle por todas las palabras
+            
+               //bucle por todas las palabras
                //leo cada dato del archivo
                String mS = st.nextToken();
                String velMaxS = st.nextToken();
                String segS = st.nextToken();
                 
-                //convierto los datos a tipo DOUBLE
+               //convierto los datos a tipo DOUBLE
                double m = Double.parseDouble(mS);
                double velMax = Double.parseDouble(velMaxS);
                double seg = Double.parseDouble(segS);
 
-                if(seg < 0) {
-                    seguir = false;
-                    System.out.println("ERROR");
-                }
-                
-                if(seg == 0 && velMax == 0 && m == 0) {
-                    break;
-                }
-                
-               if(seguir == true) {
-                double velocidadMedia = (m / seg) * 3.6;
-                
-                if ( velocidadMedia <= velMax) {
-                    System.out.println("OK");
-                } else if (velocidadMedia >= velMax * 1.20){
-                    System.out.println("PUNTOS");
-                }  else {
-                    System.out.println("MULTA");
-                }  
+               if (seg < 0) {
+                   seguir = false;
+                   System.out.println("ERROR");
                }
-        
+                
+               if (seg == 0 && velMax == 0 && m == 0) {
+                   break;
+               }
+                
+               if (seguir == true) {
+                    double velocidadMedia = (m / seg) * 3.6;
+                
+                    if (velocidadMedia <= velMax) {
+                        System.out.println("OK");
+                    } else if (velocidadMedia >= velMax * 1.20) {
+                        System.out.println("PUNTOS");
+                    } else {
+                        System.out.println("MULTA");
+                }  
+            }
         }
-
-}
+    }
 }
